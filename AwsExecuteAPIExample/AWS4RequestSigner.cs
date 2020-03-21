d9bc26541b58960bc43dfd6385f96a68c188b2f1
Sajag Jain <sajag.veer@gmail.com>
2020-03-22 03:15:12 +0530

    AWS Signer with x-api-key support

---
+++ AwsExecuteAPIExample/AWS4RequestSigner.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AwsExecuteAPIExample
+{
+    public class AWS4RequestSigner
+    {
+        private readonly string _accessKey;
+        private readonly string _secretKey;
+        private readonly SHA256 _sha256;
+        private const string ALGORITHM = "AWS4-HMAC-SHA256";
+        private const string EMPTY_STRING_HASH = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+
+        public AWS4RequestSigner(string accessKey, string secretKey)
+        {
+
+            if (string.IsNullOrEmpty(accessKey))
+            {
+                throw new ArgumentOutOfRangeException(nameof(accessKey), accessKey, "Not a valid access_key.");
+            }
+
+            if (string.IsNullOrEmpty(secretKey))
+            {
+                throw new ArgumentOutOfRangeException(nameof(secretKey), secretKey, "Not a valid secret_key.");
+            }
+
+            _accessKey = accessKey;
+            _secretKey = secretKey;
+            _sha256 = SHA256.Create();
+        }
+
+        private string Hash(byte[] bytesToHash)
+        {
+            var result = _sha256.ComputeHash(bytesToHash);
+            return ToHexString(result);
+        }
+
+        private static byte[] HmacSha256(byte[] key, string data)
+        {
+            var hashAlgorithm = new HMACSHA256(key);
+
+            return hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(data));
+        }
+
+        private static byte[] GetSignatureKey(string key, string dateStamp, string regionName, string serviceName)
+        {
+            var kSecret = Encoding.UTF8.GetBytes("AWS4" + key);
+            var kDate = HmacSha256(kSecret, dateStamp);
+            var kRegion = HmacSha256(kDate, regionName);
+            var kService = HmacSha256(kRegion, serviceName);
+            var kSigning = HmacSha256(kService, "aws4_request");
+            return kSigning;
+        }
+
+        private static string ToHexString(IReadOnlyCollection<byte> array)
+        {
+            var hex = new StringBuilder(array.Count * 2);
+            foreach (var b in array)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            return hex.ToString();
+        }
+
+        public async Task<HttpRequestMessage> Sign(HttpRequestMessage request, string service, string region,string x_api_key = "")
+        {
+            if (string.IsNullOrEmpty(service))
+            {
+                throw new ArgumentOutOfRangeException(nameof(service), service, "Not a valid service.");
+            }
+
+            if (string.IsNullOrEmpty(region))
+            {
+                throw new ArgumentOutOfRangeException(nameof(region), region, "Not a valid region.");
+            }
+
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            if (request.Headers.Host == null)
+            {
+                request.Headers.Host = request.RequestUri.Host;
+            }
+            
+            var content = new byte[0];
+            if (request.Content != null) {
+                content = await request.Content.ReadAsByteArrayAsync();
+            }
+            
+            var payloadHash = EMPTY_STRING_HASH;
+            if (content.Length != 0) {
+                payloadHash = Hash(content);
+            }
+
+            if (request.Headers.Contains("x-amz-content-sha256") == false)
+                request.Headers.Add("x-amz-content-sha256", payloadHash);
+
+            var t = DateTimeOffset.UtcNow;
+            var amzDate = t.ToString("yyyyMMddTHHmmssZ");
+            request.Headers.Add("x-amz-date", amzDate);
+            var dateStamp = t.ToString("yyyyMMdd");
+
+            if (x_api_key != "")
+                request.Headers.Add("x-api-key", x_api_key);
+
+            var canonicalRequest = new StringBuilder();
+            canonicalRequest.Append(request.Method + "\n");
+           
+            canonicalRequest.Append(string.Join("/", request.RequestUri.AbsolutePath.Split('/').Select(Uri.EscapeDataString)) + "\n");
+
+            var canonicalQueryParams = GetCanonicalQueryParams(request);
+
+            canonicalRequest.Append(canonicalQueryParams + "\n");
+
+            var signedHeadersList = new List<string>();
+
+            foreach (var header in request.Headers.OrderBy(a => a.Key.ToLowerInvariant(), StringComparer.OrdinalIgnoreCase))
+            {
+                canonicalRequest.Append(header.Key.ToLowerInvariant());
+                canonicalRequest.Append(":");
+                canonicalRequest.Append(string.Join(",", header.Value.Select(s => s.Trim())));
+                canonicalRequest.Append("\n");
+                signedHeadersList.Add(header.Key.ToLowerInvariant());
+            }
+
+            canonicalRequest.Append("\n");
+
+            var signedHeaders = string.Join(";", signedHeadersList);
+
+            canonicalRequest.Append(signedHeaders + "\n");
+            canonicalRequest.Append(payloadHash);
+            
+            var credentialScope = $"{dateStamp }/{region}/{service}/aws4_request";
+                       
+            var stringToSign = $"{ALGORITHM}\n{amzDate}\n{credentialScope}\n" + Hash(Encoding.UTF8.GetBytes(canonicalRequest.ToString()));
+
+            var signingKey = GetSignatureKey(_secretKey, dateStamp , region, service);
+            var signature = ToHexString(HmacSha256(signingKey, stringToSign));
+            
+            request.Headers.TryAddWithoutValidation("Authorization", $"{ALGORITHM} Credential={_accessKey}/{credentialScope}, SignedHeaders={signedHeaders}, Signature={signature}");
+
+            return request;
+        }
+
+        private static string GetCanonicalQueryParams(HttpRequestMessage request)
+        {
+            var values=new SortedDictionary<string, string>();
+
+            var querystring = UriExtensions.ParseQueryString(request.RequestUri);
+            foreach(var key in querystring.AllKeys)
+            {
+                if (key == null)//Handles keys without values
+                {
+                    values.Add(Uri.EscapeDataString(querystring[key]), $"{Uri.EscapeDataString(querystring[key])}=");
+                }
+                else
+                {
+                    // Query params must be escaped in upper case (i.e. "%2C", not "%2c").
+                    values.Add(Uri.EscapeDataString(key), $"{Uri.EscapeDataString(key)}={Uri.EscapeDataString(querystring[key])}");
+                }
+            }
+            
+            var queryParams = values.Select(a => a.Value);
+            var canonicalQueryParams = string.Join("&", queryParams);
+            return canonicalQueryParams;
+        }
+    }
+}
---
+++ AwsExecuteAPIExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwsExecuteAPIExample
+{
+    class Program
+    {
+        private readonly string _accessKey;
+        private readonly string _secretKey;
+        private readonly string _service;
+        private readonly string _region;
+        private readonly Uri _requestUri;
+        private readonly string _json;
+        private readonly string _x_api_key;
+
+        public Program()
+        {
+            _accessKey = "<your_access_key>";
+            _secretKey = "<your_secret_key>";
+            _service = "execute-api";
+            _region = "us-east-2";
+            _requestUri = new Uri("your_api_gateway_endpoint");
+            //Make sure to type your json with double quotes. Single quotes does not work while parsing the data
+            _json = "{\"type\":\"dog\",\"price\":2008}";
+            _x_api_key = "<api_key_value_if_configured>";
+        }
+
+        static void Main(string[] args)
+        {
+            new Program().TestSigner().GetAwaiter().GetResult();
+            
+        }
+
+        public async Task TestSigner()
+        {
+            var signer = new AWS4RequestSigner(_accessKey, _secretKey);
+            var content = new StringContent(_json, Encoding.UTF8, "application/json");
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = _requestUri,
+                Content = content
+            };
+
+            request = await signer.Sign(request, _service, _region,_x_api_key);
+
+            var client = new HttpClient();
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
